0b2b75ee4a79eaa84d3637dcc45f441798fb6771
shree14 <shrinivas.deshmukh11@gmail.com>
2017-03-19 18:12:19 +0530

    Initial commit

---
+++ menu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+
+
+
+			
+				<div class="container">
+						
+					<!-- Logo -->
+						<h1><a href="#" id="logo">TechnoPandas</a></h1>
+					
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li>
+									<a href="">AboutUs</a>
+									</li>
+											
+								<li><a href="left-sidebar.php">Login</a>
+								<ul>
+												<li><a href="#">Admin Login</a></li>
+												<li><a href="#">Doctor Login</a></li>
+												<li><a href="#">User Login</a></li>
+												
+											</ul>
+											</li>
+								<li><a href="right-sidebar.php">ContactUs</a></li>
+								<li><a href="no-sidebar.php">Cancer A-Z</a></li>
+							</ul>
+						</nav>
+
+
+					<!-- Banner -->
+						<div id="banner">
+							<div class="container">
+								<section>
+									<header class="major">
+										<h2>Welcome to TechnoPandas!</h2>
+										<span class="byline">&hellip; “
+
+To prevent a cancer, than have to diagnose and treat and cure it, because then we save the patient all of the problems of treatment."</span>
+									</header>
+									<a href="#" class="button alt">Sign Up</a>
+								</section>			
+							</div>
+						</div>
+
+				</div>
+			
+
+<?php
+	
+?>
+
+</html>
